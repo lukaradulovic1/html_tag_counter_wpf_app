dfd841b449b007b73d86bd5fc6e151a4f3399b23
lukaradulovic1 <luka.radulovic.011@gmail.com>
2023-04-15 17:01:09 +0200

    Initial commit, added database-first model.

---
+++ database_first_app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_first_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
