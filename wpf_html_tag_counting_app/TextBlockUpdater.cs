a207f142597e28907b2a94689a14f43248bab794
lukaradulovic1 <luka.radulovic.011@gmail.com>
2023-04-27 13:56:00 +0200

    Added class TextUpdater which updates textblock after each item in the dictionary is added, same for Failed downloads dictionary

---
+++ wpf_html_tag_counting_app/TextBlockUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace wpf_html_tag_counting_app
+{
+
+    public class TextBlockUpdater
+    {
+        private TextBlock _textBlock;
+        public TextBlockUpdater(TextBlock textBlock)
+        {
+            _textBlock = textBlock;
+        }
+        public void UpdateText(string text)
+        {
+            // Use the Dispatcher to update the TextBlock on the UI thread
+            _textBlock.Dispatcher.Invoke(() =>
+            {
+                _textBlock.Text = text;
+            });
+        }
+
+        public void ResetApp()
+        {
+            _textBlock.Text = "";
+        }
+    }
+}
--- wpf_html_tag_counting_app/UrlTagCounter.cs
+++ wpf_html_tag_counting_app/UrlTagCounter.cs
@@ -5,13 +5,15 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace wpf_html_tag_counting_app
 {
     public class UrlTagCounter
     {
 
-        public void ProcessFile()
+        public void ProcessFile(TextBlock textBlock)
         {
             OpenFileDialog openFileDialog;
             InitiateDialogWindow(out openFileDialog, out bool? result);
@@ -23,10 +25,10 @@ namespace wpf_html_tag_counting_app
                 Dictionary<string, int> urlTagCountDictionary = new Dictionary<string, int>();
                 Dictionary<string, string> failedDownloads = new Dictionary<string, string>();
 
-                ProcessPair(fileContents, urlTagCountDictionary, failedDownloads);
+                ProcessPair(fileContents, urlTagCountDictionary, failedDownloads, textBlock);
             }
         }
-        private void InitiateDialogWindow(out OpenFileDialog openFileDialog, out bool? result)
+        private static void InitiateDialogWindow(out OpenFileDialog openFileDialog, out bool? result)
         {
             openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Text files (*.txt)|*.txt";
@@ -40,41 +42,64 @@ namespace wpf_html_tag_counting_app
             List<string> fileContents = ReadTextFile(filePath);
             return fileContents;
         }
-        private List<string> ReadTextFile(string filePath)
+        private static List<string> ReadTextFile(string filePath)
         {
             List<string> fileContentsList = File.ReadAllLines(filePath).ToList();
             return fileContentsList;
         }
-        private void ProcessPair(List<string> fileContents, Dictionary<string, int> urlTagCountDictionary, Dictionary<string, string> failedDownloads)
+        private void ProcessPair(List<string> fileContents, Dictionary<string, int> urlTagCountDictionary, Dictionary<string, string> failedDownloads, TextBlock textBlock)
         {
             HtmlParser htmlParser = new HtmlParser();
             foreach (var item in fileContents)
             {
-                try
+                AddHtmlOutputToDictionary(urlTagCountDictionary, failedDownloads, htmlParser, item);
+                DisplayDictionariesOnTextBlock(urlTagCountDictionary, failedDownloads, textBlock);
+            }
+        }
+
+        private static void AddHtmlOutputToDictionary(Dictionary<string, int> urlTagCountDictionary, Dictionary<string, string> failedDownloads, HtmlParser htmlPageParser, string item)
+        {
+            try
+            {
+                var htmlContent = htmlPageParser.DownloadUrlHtmlToString(item);
+                var numATags = htmlPageParser.CountTagsInHtmlString(htmlContent);
+
+                if (htmlContent != null)
                 {
-                    var htmlContent = htmlParser.DownloadUrlHtmlToString(item);
-                    var numATags = htmlParser.CountTagsInHtmlString(htmlContent);
-
-                    if (htmlContent != null)
+                    if (numATags > 0)
                     {
-                        if (numATags > 0)
-                        {
-                            urlTagCountDictionary.Add(item, numATags);
-                        }
+                        urlTagCountDictionary.Add(item, numATags);
+                    }
 
-                        else
-                        {
-                            failedDownloads.Add(item, "Tag count is 0 due to HTML file not providing tags");
-                        }
+                    else
+                    {
+                        failedDownloads.Add(item, "Tag count is 0 due to HTML file not providing tags");
                     }
                 }
-                catch (Exception ex)
-                {
-                    failedDownloads.Add(item, ex.Message);
-                }
-                var concatenatedStringSuccess = urlTagCountDictionary.Select(kv => $"{kv.Key}: {kv.Value}");
-                var concatenatedStringFailed = failedDownloads.Select(kv => $"{kv.Key}: {kv.Value}");
+            }
+            catch (Exception ex)
+            {
+                failedDownloads.Add(item, ex.Message);
+            }
+        }
+        private static void DisplayDictionariesOnTextBlock(Dictionary<string, int> urlTagCountDictionary, Dictionary<string, string> failedDownloads, TextBlock textBlock)
+        {
+            if (textBlock != null)
+            {
+                TextBlockUpdater textBlockUpdater = new TextBlockUpdater(textBlock);
+                StringBuilder sb = new StringBuilder();
 
+                foreach (var kvp in urlTagCountDictionary)
+                {
+                    sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+                }
+
+                foreach (var kvp in failedDownloads)
+                {
+                    sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+                }
+                textBlockUpdater.UpdateText(sb.ToString());
+                Application.Current.Dispatcher.Invoke(() => { }, System.Windows.Threading.DispatcherPriority.Background);
             }
         }
 
