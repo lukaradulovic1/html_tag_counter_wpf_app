6f925bf79707f94dd02b62dbb9ee0dbafa143445
lukaradulovic1 <luka.radulovic.011@gmail.com>
2023-04-27 10:10:56 +0200

    Implemented HtmlParser class, implemented UrlTagCounter class

---
+++ wpf_html_tag_counting_app/HtmlParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace wpf_html_tag_counting_app
+{
+    public class HtmlParser
+    {
+        public string DownloadUrlHtmlToString(string txtUrlAdress)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                int maxRetries = 5;
+                int retryCount = 0;
+                int delayToRetry = 5000;
+
+                while (retryCount < maxRetries)
+                {
+                    try
+                    {
+                        return webClient.DownloadString(txtUrlAdress);
+                    }
+                    catch (WebException e)
+                    {
+                        if (e.Status == WebExceptionStatus.Timeout)
+                        {
+                            Thread.Sleep(delayToRetry);
+                            retryCount++;
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+                throw new Exception($"Maximum retry count of {maxRetries} has been reached.");
+
+            }
+        }
+        public int CountTagsInHtmlString(string htmlString)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlString);
+
+            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//a[text()] | //a[not(text())]");
+
+            if (linkNodes != null)
+            {
+                return linkNodes.Count;
+            }
+            return 0;
+        }
+    }
+}
---
+++ wpf_html_tag_counting_app/UrlTagCounter.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_html_tag_counting_app
+{
+    public class UrlTagCounter
+    {
+
+        public void ProcessFile()
+        {
+            OpenFileDialog openFileDialog;
+            InitiateDialogWindow(out openFileDialog, out bool? result);
+
+            if (result == true)
+            {
+                List<string> fileContents = ReadTextContents(openFileDialog);
+
+                Dictionary<string, int> urlTagCountDictionary = new Dictionary<string, int>();
+                Dictionary<string, string> failedDownloads = new Dictionary<string, string>();
+
+                ProcessPair(fileContents, urlTagCountDictionary, failedDownloads);
+            }
+        }
+        private void InitiateDialogWindow(out OpenFileDialog openFileDialog, out bool? result)
+        {
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog.DefaultExt = ".txt";
+
+            result = openFileDialog.ShowDialog();
+        }
+        private List<string> ReadTextContents(OpenFileDialog openFileDialog)
+        {
+            string filePath = openFileDialog.FileName;
+            List<string> fileContents = ReadTextFile(filePath);
+            return fileContents;
+        }
+        private List<string> ReadTextFile(string filePath)
+        {
+            List<string> fileContentsList = File.ReadAllLines(filePath).ToList();
+            return fileContentsList;
+        }
+        private void ProcessPair(List<string> fileContents, Dictionary<string, int> urlTagCountDictionary, Dictionary<string, string> failedDownloads)
+        {
+            HtmlParser htmlParser = new HtmlParser();
+            foreach (var item in fileContents)
+            {
+                try
+                {
+                    var htmlContent = htmlParser.DownloadUrlHtmlToString(item);
+                    var numATags = htmlParser.CountTagsInHtmlString(htmlContent);
+
+                    if (htmlContent != null)
+                    {
+                        if (numATags > 0)
+                        {
+                            urlTagCountDictionary.Add(item, numATags);
+                        }
+
+                        else
+                        {
+                            failedDownloads.Add(item, "Tag count is 0 due to HTML file not providing tags");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    failedDownloads.Add(item, ex.Message);
+                }
+                var concatenatedStringSuccess = urlTagCountDictionary.Select(kv => $"{kv.Key}: {kv.Value}");
+                var concatenatedStringFailed = failedDownloads.Select(kv => $"{kv.Key}: {kv.Value}");
+
+            }
+        }
+
+    }
+}
