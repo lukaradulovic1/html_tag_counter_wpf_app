9fe33ba0a57b43276c385a38f4fd47717ac76b1d
lukaradulovic1 <luka.radulovic.011@gmail.com>
2023-04-30 10:04:09 +0200

    Made field readonly.

--- wpf_html_tag_counting_app/TextBlockUpdater.cs
+++ wpf_html_tag_counting_app/TextBlockUpdater.cs
@@ -5,22 +5,22 @@ namespace wpf_html_tag_counting_app
 
     public class TextBlockUpdater
     {
-        private TextBlock textBlock;
+        private readonly TextBlock displayTextBlock;
         public TextBlockUpdater(TextBlock textBlock)
         {
-            textBlock = textBlock;
+            displayTextBlock = textBlock;
         }
         public void UpdateText(string text)
         {
             // Use the Dispatcher to update the TextBlock on the UI thread
-            textBlock.Dispatcher.Invoke(() =>
+            displayTextBlock.Dispatcher.Invoke(() =>
             {
-                textBlock.Text = text;
+                displayTextBlock.Text = text;
             });
         }
         public void ResetApp()
         {
-            textBlock.Text = "";
+            displayTextBlock.Text = "";
         }
     }
 }
